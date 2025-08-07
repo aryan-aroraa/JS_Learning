e97a6afae488ea724a2a5b75100377794f96f1c2
aryan-aroraa <138298702+aryan-aroraa@users.noreply.github.com>
2025-08-07 21:17:57 +0000

    Nullish Coelescing Operator (??)

---
+++ 01_BASICS/13_nullish_op.js
@@ -0,0 +1,9 @@
+// Nullish Coelescing Operator (??): numm undefined
+
+let var1;
+// var1 = 5 ?? 10 //5
+// var1 = null ?? 10 //10
+// var1 = undefined ?? 10 //10
+// var1 = null ?? 5 ?? 10 //5
+
+console.log(var1);
